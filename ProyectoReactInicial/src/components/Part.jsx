d50b7d3e312119c218c2a67f1ed4d9985c316665
Giovanii7u7 <giovaniarango6@gmail.com>
2025-11-01 23:21:05 -0600

    Contador automático con setTimeout

--- ProyectoReactInicial/src/components/Part.jsx
+++
@@ -1,9 +0,0 @@
-const Part = (props) => {
-  return (
-    <p>
-      {props.partName} {props.exercises}
-    </p>
-  )
-}
-
-export default Part
--- t3_react_estado_eventos/src/App.jsx
+++ t3_react_estado_eventos/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+};
 
-export default App
+const App = () => {
+  const name = "Peter";
+  const age = 10;
+
+  // Estado para el contador
+  const [counter, setCounter] = useState(0);
+
+  // Incrementa el contador cada segundo
+  setTimeout(() => setCounter(counter + 1), 1000);
+
+  console.log("rendering...", counter);
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+
+      <h2>Counter: {counter}</h2>
+    </div>
+  );
+};
+
+export default App;
